2a372d714af53910a1ce3bfa8777b91d0bd4e585
Joseph <mwanikij118@gmail.com>
2025-06-06 15:35:33 +0300

    Adds index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+function divide(a, b) {
+    return 2000 / 100;
+}
+const square = (parameter1) => parameter1 * parameter1;
+
+const add = (parameter1, parameter2) => parameter1 + parameter2;
+
+
+
+
+
